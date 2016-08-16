3ecce3e1c9698272b728b30ada70eafe0237c527
Matteo Maronati <matteo@gentleman-studio.de>
2016-08-16 14:29:55 +0200

    Added and implemented event emitter for settings change

--- src/js/classes/Settings.js
+++ src/js/classes/Settings.js
@@ -9,7 +9,9 @@
 // =======================================================================
 
 import featurejs from 'featurejs';
-import $ from 'jquery';
+import $ from 'jquery'
+import ee from 'event-emitter'
+
 
 
 // Settings class
@@ -22,11 +24,16 @@ class Settings {
     // Initialises all properties
     this.init()
 
+    // Initialises the event emitters
+    this.addEmitters()
+
     // Add event listeners to update properties
     this.addEventListeners()
 
   }
 
+
+  // Initialises all properties
   init() {
 
     // window size
@@ -84,6 +91,16 @@ class Settings {
 
   }
 
+  // Adding event emitters for the different properties changes
+  addEmitters() {
+    this.emitters = {
+      window : ee({}), function(){},
+      scroll : ee({}), function(){},
+      mouse : ee({}), function(){}
+    }
+  }
+
+  // Add event listener and emit events
   addEventListeners() {
 
     // Scroll
@@ -96,6 +113,7 @@ class Settings {
       if (this.scroll.y == null) {
         this.scroll.y = 0
       }
+      this.emitters.scroll.emit('change')
     });
 
     // Resize (with timeout)
@@ -104,7 +122,7 @@ class Settings {
       this.resize.timeout = setTimeout(() => {
         this.window.x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth
         this.window.y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight
-        console.log(this);
+        this.emitters.window.emit('change')
       }, this.resize.delay);
     });
 
@@ -112,6 +130,7 @@ class Settings {
     window.addEventListener('mousemove', (e) => {
       this.mouse.x = e.clientX
       this.mouse.y = e.clientY
+      this.emitters.mouse.emit('change')
     });
   }
 
