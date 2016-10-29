83ce659d01ecc3b7e87bf0feb428d55e86ec1ba8
Matteo Maronati <matteo@gentleman-studio.de>
2016-10-29 15:10:56 +0200

    Up: ported iamges task and docked to base task

--- gulp/tasks/_base.js
+++ gulp/tasks/_base.js
@@ -3,9 +3,10 @@ import clean from './clean'
 import bundler from './bundler'
 import server from './server'
 import fonts from './fonts'
+import images from './images'
 
 
 gulp.task('base', gulp.series(
   gulp.parallel(clean),
-  gulp.parallel(bundler, fonts, server)
+  gulp.parallel(bundler, fonts, server, images)
 ))
