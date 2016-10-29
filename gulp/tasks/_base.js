afafc4de90b6c008ca09c701e3962f95e84cd461
Matteo Maronati <matteo@gentleman-studio.de>
2016-10-29 15:04:23 +0200

    Up: docked fonts task to base task

--- gulp/tasks/_base.js
+++ gulp/tasks/_base.js
@@ -2,10 +2,10 @@ import gulp from 'gulp'
 import clean from './clean'
 import bundler from './bundler'
 import server from './server'
-
+import fonts from './fonts'
 
 
 gulp.task('base', gulp.series(
-  gulp.parallel(clean, server),
-  gulp.parallel(bundler)
+  gulp.parallel(clean),
+  gulp.parallel(bundler, fonts, server)
 ))
