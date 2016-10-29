4176476ffa7a265eb5083e535836e3f93bffd12d
Matteo Maronati <matteo@gentleman-studio.de>
2016-10-29 17:11:53 +0200

    Up: init proting for watcher

--- gulp/tasks/_watch.js
+++ gulp/tasks/_watch.js
@@ -1,9 +1,2 @@
 import gulp from 'gulp'
 import server from './server'
-
-gulp.task('watch', gulp.series(
-  function setenv (done) {
-    process.env.WATCH = 'true'
-    done()
-  }, 'base', server)
-)
