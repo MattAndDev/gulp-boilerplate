d91cf565dc34d8481cdadb1de9155898e96758e7
Matteo Maronati <matteo@gentleman-studio.de>
2016-11-02 22:50:56 +0100

    Up: stable twig templating test

--- gulp/tasks/markup.js
+++ gulp/tasks/markup.js
@@ -1,10 +1,9 @@
 import gulp from 'gulp'
 import config from '../config'
 import browserSync from 'browser-sync'
-import include from 'gulp-file-include'
-import rename from 'gulp-rename'
-import minifyHTML from 'gulp-minify-html'
+import twig from 'gulp-twig'
 import handleErrors from '../util/handleErrors'
+import minifyHTML from 'gulp-minify-html'
 
 // ============================================
 // Markup task
@@ -15,10 +14,7 @@ import handleErrors from '../util/handleErrors'
 
 function markup () {
   return gulp.src(config.markup.src)
-    .pipe(include(config.markup.settings))
-    .on('error', handleErrors)
-    .pipe(rename({extname: ''}))
-    .pipe(rename({extname: '.html'}))
+    .pipe(twig())
     .pipe(minifyHTML({spare: true}))
     .pipe(gulp.dest(config.markup.dest))
     .pipe(browserSync.reload({stream: true}))
