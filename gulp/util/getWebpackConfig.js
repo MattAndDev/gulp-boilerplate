b957865f7b2f4756e3f3a97330e3c23eb1095d9b
Matteo Maronati <matteo@gentleman-studio.de>
2017-04-04 20:42:44 +0200

    Up: adding utility functions for gulp webpack

---
+++ gulp/util/getWebpackConfig.js
@@ -0,0 +1,42 @@
+/* eslint import/no-extraneous-dependencies: 0 */
+
+import config from '../config.js'
+import webpack from 'webpack'
+
+module.exports = (env) => {
+  console.log(env)
+  console.log(config.webpack)
+  if (!config.webpack.plugins) {
+    config.webpack.plugins = []
+  }
+
+  if (env === 'build') {
+    config.webpack.devtool = 'inline-source-map'
+  }
+
+  if (env === 'watch') {
+    config.webpack.devtool = 'inline-source-map'
+
+    Object.keys(config.webpack.entry).forEach((key) => {
+      const entry = config.webpack.entry[key]
+      config.webpack.entry[key] = ['webpack-hot-middleware/client?&reload=true'].concat(entry)
+    })
+
+    config.webpack.plugins.push(
+      new webpack.HotModuleReplacementPlugin()
+    )
+  }
+
+  if (env === 'prod') {
+    config.webpack.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify('production'),
+        },
+      }),
+      new webpack.optimize.UglifyJsPlugin()
+    )
+  }
+  console.log(config.webpack)
+  return config.webpack
+}
